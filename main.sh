ccdee3a757b6be2cdc547f9aa749018c526a1cfa
rdjtbsgxxhgmtj-hotmail-com <126742562+rdjtbsgxxhgmtj-hotmail-com@users.noreply.github.com>
2023-03-06 11:34:57 +0800

    d

--- main.sh
+++ main.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
-###################################
-youtube-channel: 云白科技
-author: yunbaitech
-###################################
+echo "###################################"
+echo "youtube-channel: 云白科技"
+echo "author: yunbaitech"
+echo "###################################"
 echo "开始安装"
 sleep 2
 echo "安装宝塔面板"
 if [ -f /usr/bin/curl ];then curl -sSO https://download.bt.cn/install/install_panel.sh;else wget -O install_panel.sh https://download.bt.cn/install/install_panel.sh;fi;bash install_panel.sh ed8484bec
 
-echo
+echo "第二步,破解宝塔面板"
+
+
