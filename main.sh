b938e5e9dbfe03ae924e8cacf948a30d17932289
rdjtbsgxxhgmtj-hotmail-com <126742562+rdjtbsgxxhgmtj-hotmail-com@users.noreply.github.com>
2023-03-06 13:13:24 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,9 +10,9 @@ echo "安装宝塔面板"
 if [ -f /usr/bin/curl ];then curl -sSO https://download.bt.cn/install/install_panel.sh;else wget -O install_panel.sh https://download.bt.cn/install/install_panel.sh;fi;bash install_panel.sh ed8484bec
 
 echo "第二步,破解宝塔面板"
-
+sleep 3
 bash btvip.sh
-
+sleep 2
 echo "last step "
 
 rm -f /www/server/panel/data/admin_path.pl
